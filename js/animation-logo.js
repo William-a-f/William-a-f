e79664eafd082a2408b2b5f14aa1dbdb91c1effb
William-a-f <149526402+William-a-f@users.noreply.github.com>
2024-07-09 12:10:44 +0100

    animacion de menu y social en index

--- js/animation-logo.js
+++ js/animation-logo.js
@@ -1,13 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
-	const path =
-	window.location.pathname;
-	if (path === '/' || path.endsWith('/index.html')) {
 		const logo =
 		document.querySelector('.home-page .logo-img');
-      if (logo) {
+		const menu =
+		document.querySelector('.home-page .nav');
+		const footer =
+		document.querySelector('.home-page .social');
+
+	
+	if (window.location.pathname === '/' || window.location.pathname.endsWith('/index.html')) {
 				setTimeout(() => {
 						logo.classList.add('visible');
     		}, 100);
+
+				setTimeout(() => {
+					menu.classList.remove('hidden');
+					menu.classList.add('visible');
+
+				}, 1500);
+
+				setTimeout(() =>{
+					footer.classList.remove('hidden');
+					footer.classList.add('visible');
+				}, 2500);
 			}
-		}
 });
