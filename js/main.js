a8738d2e7a4efc39432fb69a25ea23c35f4e44e2
William-a-f <149526402+William-a-f@users.noreply.github.com>
2024-08-05 13:14:36 +0100

    limpio recursos main js y css

--- js/main.js
+++ js/main.js
@@ -8,18 +8,6 @@ window.addEventListener('load', function() {
 	document .body.classList.remove('no-scroll');
 });
 
-// // animacion de entrada laterales release
-// window.addEventListener('load', function() {
-//   document.querySelector('.release-text').classList.add('animate');
-//   document.querySelector('.release-img').classList.add('animate');
-// });
-
-// // animacion de entrada laterales biografia
-// window.addEventListener('load', function() {
-//   document.querySelector('.bio__img-back').classList.add('animate');
-//   document.querySelector('.bio-text').classList.add('animate');
-// });
-
 // navToggle- mostrar
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
